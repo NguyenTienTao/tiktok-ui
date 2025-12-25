a928d18f4ed5701dd1e1960d335e317d26823835
NguyenTienTao <tientao05042004@gmail.com>
2025-12-25 21:48:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes, Link } from "react-router";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import News from "./pages/News";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        <li>
+                            <Link to="/news">News</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/news" element={<News />} />
+                </Routes>
+            </BrowserRouter>
+            ;
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/News.js
@@ -0,0 +1,5 @@
+const News = () => {
+    return <h2>News Page</h2>;
+};
+
+export default News;
